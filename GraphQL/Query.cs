5bbd74798c1814f29dc1319ee7ea711eac928e89
Aditya Kumar <aditya30394@gmail.com>
2023-02-20 01:40:59 -0800

    Add query and graphql endpoint

---
+++ GraphQL/Query.cs
@@ -0,0 +1,13 @@
+using Commander.Models;
+using CommanderGQL.Data;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Platform> GetPlatform([Service] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,10 +10,24 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("CommandConStr")));
 
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var environment = builder.Environment;
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+//app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+});
 
 app.Run();
