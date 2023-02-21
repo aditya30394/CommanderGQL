ffcdf28af6407292c73c55f954ec3c19bdbb4661
Aditya Kumar <aditya30394@gmail.com>
2023-02-20 20:34:49 -0800

    Add some grpah ql documentations

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -3,6 +3,7 @@ using CommanderGQL.Models;
 
 namespace Commander.Models
 {
+    [GraphQLDescription("Represents any software or service that has a command line interface.")]
     public class Platform
     {
         [Key]
@@ -11,6 +12,7 @@ namespace Commander.Models
         [Required]
         public string? Name { get; set; }
 
+        [GraphQLDescription("Represents the purchased and valid license key for the platform.")]
         public string? LicenseKey { get; set; }
 
         public ICollection<Command> Commands { get; set; } = new List<Command>();
